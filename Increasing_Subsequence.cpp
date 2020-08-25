2f1812a097bfb557300f13b19dc2560ca55d2b12
Yugant Kachroo <yugant35@gmail.com>
2020-08-26 01:23:36 +0530

    increasing subsequence updated

--- Increasing_Subsequence.cpp
+++ Increasing_Subsequence.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  vector<int> dp;
-  for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    auto it = lower_bound(dp.begin(), dp.end(), x);
-    if (it == dp.end()) {
-      dp.push_back(x);
-    } else {
-      *it = x;
-    }
-  }
-  cout << dp.size() << endl;
+    int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++) cin >> a[i];
+        vector<int> v;
+        v.push_back(a[0]);
+        for(int i = 1; i < n; i++) {
+                if(a[i] > v.back()) v.push_back(a[i]);
+                else {
+                        int it = lower_bound(v.begin(), v.end(), a[i]) - v.begin();
+                        v[it] = a[i];
+                }
+        }
+        cout << v.size() << endl;
+        return 0;
 }
