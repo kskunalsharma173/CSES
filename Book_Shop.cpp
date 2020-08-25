2ae41bcf586a881fc419adc29da45995a5464aaf
Yugant Kachroo <yugant35@gmail.com>
2020-08-25 22:28:12 +0530

    book shop done

---
+++ Book_Shop.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> price(n), pages(n);
+    for(int i = 0;i<n;i++) cin >> price[i];
+    for(int i = 0;i<n;i++) cin >> pages[i];
+    int dp[n + 1][x + 1];
+    for(int i = 0;i<=x;i++) dp[0][i] = 0;
+    for(int i = 0;i<=n;i++) dp[i][0] = 0;
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=x;j++){
+            if(price[i - 1] <= j){
+                dp[i][j] = max(pages[i - 1] + dp[i - 1][j - price[i - 1]], dp[i - 1][j]);
+            }
+            else dp[i][j] = dp[i - 1][j];
+        }
+    }
+    cout << dp[n][x];
+}
