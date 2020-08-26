adf3b3427c1daee40e2d4d672dfe0880ec7d7dd2
Yugant Kachroo <yugant35@gmail.com>
2020-08-26 21:12:25 +0530

    two knights solved

--- Introductory Problems/1072_Two_Knights.cpp
+++ Introductory Problems/1072_Two_Knights.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+  long long int n,i;
+  cin >> n;
+  for (i = 1; i <= n; i++) {
+    long long int a = i * i * (i * i - 1) / 2;
+    long long int b = 2 * (i - 2) * (2 * (i - 4) + 6);
+    cout << a - b << endl;
+  }
+  return 0;
+}
