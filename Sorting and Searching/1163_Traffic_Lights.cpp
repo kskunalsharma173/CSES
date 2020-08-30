b65ec4fda3bc8c26c9f2459014a1652feb550257
Yugant Kachroo <yugant35@gmail.com>
2020-08-30 19:43:13 +0530

    traffic lights code updated

--- Sorting and Searching/1163_Traffic_Lights.cpp
+++ Sorting and Searching/1163_Traffic_Lights.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define ll long long
-
-const int N = 1e5 + 6;
-const int MOD = 1e9 + 7;
-
-int32_t main()
+#define _fi(n) for(int i = 0;i < n;i++)
+int main()
 {
-    ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
     cin.tie(0);
-    map<int, int> m;
-    set<int> s;
-    int x, n; cin >> x >> n;
+
+    long long int x, n;
+    cin>>x>>n;
+    set<long long int>s;
     s.insert(0);
     s.insert(x);
-    m[x] = 1;
-    for(int i = 0; i < n; i++) {
-        int y; cin >> y;
-        auto it = s.lower_bound(y);
-        int r = *it;
-        it--;
-        int l = *it;
-        s.insert(y);
-        m[r - l]--;
-        if(m[r - l] == 0)
-            m.erase(r - l);
-        ++m[y - l];
-        ++m[r - y];
-        cout << ((--m.end()) -> first) << " ";
+
+    multiset<long long int, greater<long long int>>ans;
+    ans.insert(x);
+    while(n){
+        long long int p;cin>>p;
+        s.insert(p);
+        auto it = s.lower_bound(p);
+        auto left = it;
+        auto right = it;
+        left--;
+        right++;
+
+        ans.erase(ans.find(*right-*left));
+        ans.insert(*right-p);
+        ans.insert(p-*left);
+        n--;
+        cout << *ans.begin()<<" ";
     }
+
     return 0;
 }
