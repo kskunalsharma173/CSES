5b2141d20b87aaee5754b798ed85f094dcca1312
Yugant Kachroo <yugant35@gmail.com>
2020-08-31 19:17:50 +0530

    subarray sums 2 solved

---
+++ Sorting and Searching/1661_Subarray_Sums_2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long int n,sum;
+    cin>>n>>sum;
+    long long int a[n+1];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    map<long long int, long long int> map;
+    long long int total=0,res=0;
+    for(int i=0;i<n;i++)
+    {
+        total+=a[i];
+        if(total==sum)
+            res++;
+       if(map.find(total-sum)!=map.end())
+           res+=map[(total-sum)];
+        map[total]++;
+    }
+    cout<<res<<endl;
+	return 0;
+}
